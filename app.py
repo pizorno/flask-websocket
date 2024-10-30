2aac93d7fa68ec1866edaa845c5c51ead2722467
Eduardo Pizorno <pizorno@gmail.com>
2024-10-30 16:48:11 -0300

    Adicionando banco de dados SQLite

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 from flask import Flask, jsonify
+from repository.database import db
+from models.payment import Payment
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.sqlite'
+app.config['SECRET_KEY'] = 'SECRET_KEY'
+db.init_app(app)
 
 @app.route('/payments/pix', methods=['POST'])
 def create_payment_pix():

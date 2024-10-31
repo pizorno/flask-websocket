0f7766df24849081bc8c709d1ce46a38351005fe
Eduardo Pizorno <pizorno@gmail.com>
2024-10-30 22:18:15 -0300

    Adicionando classe pix como método de pagamento e geração de qrcode

---
+++ payments/pix.py
@@ -0,0 +1,14 @@
+import uuid
+import qrcode
+
+class Pix:
+    def __init__(self):
+        pass
+
+    def create_payment(self):
+        bank_payment_id = uuid.uuid4()
+        hash_payment = f"hash_payment_{bank_payment_id}"
+        img = qrcode.make(hash_payment)
+        img.save(f"static/img/qrcode_payment_{bank_payment_id}.png")
+        return {"bank_payment_id": bank_payment_id,
+                "qrcode_path": f"qrcode_payment_{bank_payment_id}"}

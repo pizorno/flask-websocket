0158d1a76a0086f1679d81e673d4109bd872cef9
Eduardo Pizorno <pizorno@gmail.com>
2024-10-31 08:49:39 -0300

    Gerando qrcode e bank_payment_id e rota para retornar qrcode

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, send_file
 from repository.database import db
 from models.payment import Payment
 from datetime import datetime, timedelta
+from payments.pix import Pix
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.sqlite'
@@ -15,10 +16,18 @@ def create_payment_pix():
         return jsonify({"message": "Invalid value"}), 400
     expiration_date = datetime.now() + timedelta(minutes=30)
     new_payment = Payment(value=data['value'], expiration_date=expiration_date)
+    pix_obj = Pix()
+    data_payment_pix = pix_obj.create_payment()
+    new_payment.bank_payment_id = data_payment_pix['bank_payment_id']
+    new_payment.qrcode = data_payment_pix['qrcode_path']
     db.session.add(new_payment)
     db.session.commit()
     return jsonify({"message": "The payment has been created", "payment":
         new_payment.to_dict()})
+
+@app.route('/payments/pix/qrcode/<filename>', methods=['GET'])
+def get_image(filename):
+    return send_file(f"static/img/{filename}.png", mimetype='img/png')
 
 @app.route('/payments/pix/confirmation', methods=['POST'])
 def pix_confirmation():
--- models/payment.py
+++ models/payment.py
@@ -6,7 +6,7 @@ class Payment(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     value = db.Column(db.Float, nullable=False)
     paid = db.Column(db.Boolean, default=False, nullable=False)
-    bank_payment_id = db.Column(db.Integer, nullable=True)
+    bank_payment_id = db.Column(db.String(200), nullable=True)
     qrcode = db.Column(db.String(100), nullable=True)
     expiration_date= db.Column(db.DateTime)
 

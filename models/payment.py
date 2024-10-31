bf70943022c95f003471e0093108ebdd59871a4a
Eduardo Pizorno <pizorno@gmail.com>
2024-10-30 21:57:11 -0300

    Adicionando dados rota create payment

--- models/payment.py
+++ models/payment.py
@@ -9,3 +9,13 @@ class Payment(db.Model):
     bank_payment_id = db.Column(db.Integer, nullable=True)
     qrcode = db.Column(db.String(100), nullable=True)
     expiration_date= db.Column(db.DateTime)
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "value": self.value,
+            "paid": self.paid,
+            "bank_payment_id": self.bank_payment_id,
+            "qrcode": self.qrcode,
+            "expiration_date": self.expiration_date
+        }
